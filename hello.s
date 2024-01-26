b197c69bf89c9aca837cb9dceff63ad173fa1382
KitYan20 <kyan@bu.edu>
2024-01-26 05:54:23 +0000

    adding source file

---
+++ hello.s
@@ -0,0 +1,19 @@
+	.intel_syntax noprefix
+	.section .data
+string:
+	.ascii "Hello World\n"
+length:
+	.quad 0
+	.section .text
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, OFFSET [string]
+	mov rcx, OFFSET [length]
+	sub rcx, rsi
+	mov rdx, rcx
+	syscall
+	
+	mov rax, 60
+	mov rdi, 0
+	syscall
