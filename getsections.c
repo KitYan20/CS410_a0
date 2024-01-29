4d23dcb442ab77337e648fedc35f9bdf4a2510f0
KitYan20 <kyan@bu.edu>
2024-01-29 17:34:30 +0000

    debugging ...

--- getsections.c
+++ getsections.c
@@ -1,4 +1,4 @@
-#include "config.h"
+//#include "config.h"
 #include <unistd.h>
 #include <stdio.h>
 #include "bfd.h"
@@ -8,9 +8,10 @@ int main(int argc, char *argv[]){
     fprintf(stderr, "Usage: %s <object_file>\n",argv[1]);
     return 1;
   }
+  
   //Needs to call this function before calling any other BFD function to initialize all the internal data structures
   bfd_init();
-
+  //Opens the object file
   bfd *abfd = bfd_openr(argv[1], NULL);
 
   if (!abfd){
@@ -25,12 +26,13 @@ int main(int argc, char *argv[]){
 	       "%-25s %-20s %-20s %-20s\n",
 	       "Section Name", "VMA", "Size", "File Position");
   write(1,buffer,n);
-  for (p = abfd->sections; p!= NULL, p->next){
+  for (p = abfd->sections; p!= NULL; p->next){
     n = snprintf(buffer,sizeof(buffer),
-		 "%-25s %-181x %-20ld %20ld\n",
-		 p->name, (unsigned long)p->vma, (long)p->cooked_size, (long)p->filepos);
+		 "%-25s %-18lx %-20ld  %-20ld\n",
+		 p->name, (unsigned long)p->vma, (long)p->size, (long)p->filepos);
     write(1,buffer,n);
   }
   bfd_close(abfd);
+  
   return 0;
 }
